c10f61d5fd28c7e1990434e8d2af8d229d42433a
pawan5758 <118316058+pawanshrestha5758@users.noreply.github.com>
2023-08-18 15:32:28 +0545

    interaction finished

--- index.js
+++ index.js
@@ -14,3 +14,14 @@
 // console.log(name);
 
 //// interaction
+////alert,prompt,confirm
+// alert("hello");
+// const name = prompt("enter your name");
+// console.log(name);
+// const isvalid = confirm("are you pawan");
+// console.log(isvalid);
+
+// const a = prompt("enter the value of a");
+// const b = prompt("enter the value of b");
+// const sum = Number(a) + Number(b);
+// alert("total sum is " + sum);
