df07d8d01f9bf36d0aed884753f1be37c67e5bca
pawan5758 <118316058+pawanshrestha5758@users.noreply.github.com>
2023-08-18 15:18:04 +0545

    interaction

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// //variable
+// //traditional way
+////ES5 (Ecma Script 5)
+// var person = "ram";
+// console.log(person);
+
+// //modern way
+// //let, const
+//// ES6
+// let name = "pawan";
+// console.log(name);
+
+// const name = "heta";
+// console.log(name);
+
+//// interaction
